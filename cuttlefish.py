0ef33d07ed678427ddf7b0c823032a5e3e33ea53
Phreaklets <nospam_richard@yahoo.co.uk>
2016-10-16 15:48:35 +0100

    tweaks

--- cuttlefish.py
+++ cuttlefish.py
@@ -6,7 +6,7 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from contextlib import contextmanager
 from threading import Thread
 from Queue import Queue, Empty
-from scapy.all import load_module,Ether,IP,TCP,sniff #Import needed modules from scapy
+from scapy.all import load_module,Ether,IP,UDP,ICMP,TCP,sniff #Import needed modules from scapy
 import sys
 import sqlite3
 import datetime
@@ -110,7 +110,7 @@ def vendorlookup(ethsrc):
 
 def threaded_sniff_target(q):
     global m_finished
-    sniff(iface = m_iface, count = 0, store = 0, filter = "tcp", prn = lambda x : q.put(x))
+    sniff(iface = m_iface, count = 0, store = 0, prn = lambda x : q.put(x))
     m_finished = True
 
 def threaded_sniff():
@@ -120,85 +120,100 @@ def threaded_sniff():
     sniffer.start()
     time.sleep(1)
     t = Terminal()
+    pkt_type = ""
     
     while True:
         try:
             pkt = q.get(timeout = 1)
+            ethsrc = pkt.getlayer(Ether).src
+            ethdst = pkt.getlayer(Ether).dst
+            ipsrc = pkt.getlayer(IP).src
+            ipdst = pkt.getlayer(IP).dst
+            ttl = pkt.getlayer(IP).ttl
             if pkt.haslayer(TCP):
-                ethsrc = pkt.getlayer(Ether).src
-                ethdst = pkt.getlayer(Ether).dst
-                ipsrc = pkt.getlayer(IP).src
-                ipdst = pkt.getlayer(IP).dst
-                ttl = pkt.getlayer(IP).ttl
                 sport = pkt.getlayer(TCP).sport
                 dport = pkt.getlayer(TCP).dport
-
-                if not dbconn.isipaddrindb(ipsrc):
-                    ret_ttl = getttl(ttl)
-                    if ret_ttl  is None:
-                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "TTL", t.red("%d" % ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "TCP src port", t.yellow("%s" % sport), "TCP dst port",  t.yellow("%s" % dport)
-                    else:
-                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "OS", t.red("%s" % ret_ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "TCP src port", t.yellow("%s" % sport), "TCP dst port",  t.yellow("%s" % dport)
-                    if sport == 80 or dport == 80:
-                        if pkt.haslayer(http.HTTPRequest):
-                            http_pkt = pkt.getlayer(http.HTTPRequest)
-                            print t.move_right, t.move_right, "HTTP Method", t.yellow("%s" % http_pkt.fields['Method']), "Host", t.cyan("%s" % http_pkt.fields['Host']), "Path", t.green("%s" % http_pkt.fields['Path']),"User-Agent", t.blue("%s" % http_pkt.fields['User-Agent'])
-                        elif pkt.haslayer(http.HTTPResponse):
-                            http_pkt = pkt.getlayer(http.HTTPResponse)
-                            try:
-                                print t.move_right, t.move_right, "HTTP Response from", t.green("%s" % http_pkt.fields['Server']), "Date", t.yellow("%s" % http_pkt.fields['Date'])
-                            except:
-                                pass
-
-                    if pkt.haslayer(TLS):
-                        tls_packet = pkt.getlayer(TLS)
-                        tls_record = tls_packet.fields['records'][0]
-                        tls_record_type = tls_packet.fields['records'][0].fields['content_type']
-                        if tls_record_type == 0x17:
-                            try:
-                                tls_version = tls_record['TLS Record'].fields['version']
-                                if tls_version == 0x303:
-                                    print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.2")
-                                if tls_version == 0x302:
-                                    print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.1")
-                                if tls_version == 0x301:
-                                    print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.0")
-                                if tls_version == 0x300:
-                                    print t.move_right, t.move_right, "Application Data from SSL version", t.yellow("v3.0")
-                            except:
-                                pass
-                        elif tls_record_type == 0x16:
-                            for rec in tls_packet.fields['records']:
-                                #print rec['TLS Handshake'].fields['type']
-                                try:
-                                    if rec['TLS Handshake'].fields['type'] == 1:
-                                        #print rec['TLS Client Hello'].fields['version']
-                                        tls_clienthello_version = tls_record['TLS Client Hello'].fields['version']
-                                        if tls_clienthello_version == 0x303:
-                                            print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.2")
-                                        elif tls_clienthello_version == 0x302:
-                                            print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.1")
-                                        elif tls_clienthello_version == 0x301:
-                                            print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.0")
-                                        elif tls_clienthello_version == 0x300:
-                                            print t.move_right, t.move_right, "Client Hello version", t.yellow("v3.0")
-                                    elif rec['TLS Handshake'].fields['type'] == 2:
-                                        #print rec['TLS Server Hello'].fields['version']
-                                        tls_serverhello_version = tls_record['TLS Server Hello'].fields['version']
-                                        if tls_serverhello_version == 0x303:
-                                            print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.2")
-                                        elif tls_serverhello_version == 0x302:
-                                            print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.1")
-                                        elif tls_serverhello_version == 0x301:
-                                            print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.0")
-                                        elif tls_serverhello_version == 0x300:
-                                            print t.move_right, t.move_right, "Server Hello version", t.yellow("v3.0")
-                                except:
-                                    pass
+                pkt_type = "TCP"
+            elif pkt.haslayer(UDP):
+                sport = pkt.getlayer(UDP).sport
+                dport = pkt.getlayer(UDP).dport
+                pkt_type = "UDP"
+            elif pkt.haslayer(ICMP):
+                pkt_type = "ICMP"
+            if not dbconn.isipaddrindb(ipsrc):
+                # OS detection based on TTL
+                ret_ttl = getttl(ttl)
+                if ret_ttl  is None:
+                    if pkt_type == "TCP" or pkt_type == "UDP":
+                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "TTL", t.red("%d" % ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "%s src port" % pkt_type, t.yellow("%s" % sport), "%s dst port" % pkt_type,  t.yellow("%s" % dport)
+                    elif pkt_type == "ICMP":
+                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "TTL", t.red("%d" % ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "%s" % pkt_type
                 else:
-                    dbconn.refreshtimestamp(ethsrc)
+                    if pkt_type == "TCP" or pkt_type == "UDP":
+                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "OS", t.red("%s" % ret_ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "%s src port" % pkt_type, t.yellow("%s" % sport), "%s dst port" % pkt_type,  t.yellow("%s" % dport)
+                    elif pkt_type == "ICMP":
+                        print "T", t.blue("%s" % datetime.datetime.now().strftime('%H:%M:%S')), "MAC src addr", t.cyan("%s" % ethsrc), "MAC dst addr", t.cyan("%s" % ethdst), "OS", t.red("%s" % ret_ttl), "IP src addr", t.green("%s" % ipsrc), "IP dst addr", t.green("%s" % ipdst), "%s" % pkt_type
+                # Print out additional information if the packet contains HTTP requests or responses
+                if pkt.haslayer(http.HTTPRequest):
+                    http_pkt = pkt.getlayer(http.HTTPRequest)
+                    print t.move_right, t.move_right, "HTTP Method", t.yellow("%s" % http_pkt.fields['Method']), "Host", t.cyan("%s" % http_pkt.fields['Host']), "Path", t.green("%s" % http_pkt.fields['Path']),"User-Agent", t.blue("%s" % http_pkt.fields['User-Agent'])
+                elif pkt.haslayer(http.HTTPResponse):
+                    http_pkt = pkt.getlayer(http.HTTPResponse)
+                    try:
+                        print t.move_right, t.move_right, "HTTP Response from", t.green("%s" % http_pkt.fields['Server']), "Date", t.yellow("%s" % http_pkt.fields['Date'])
+                    except:
+                        pass
+                        
+                # Print out additional information if the packet is part of a TLS connection
+                if pkt.haslayer(TLS):
+                    tls_packet = pkt.getlayer(TLS)
+                    tls_record = tls_packet.fields['records'][0]
+                    tls_record_type = tls_packet.fields['records'][0].fields['content_type']
+                    if tls_record_type == 0x17:
+                        try:
+                            tls_version = tls_record['TLS Record'].fields['version']
+                            if tls_version == 0x303:
+                                print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.2")
+                            elif tls_version == 0x302:
+                                print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.1")
+                            elif tls_version == 0x301:
+                                print t.move_right, t.move_right, "Application Data from TLS version", t.yellow("v1.0")
+                            elif tls_version == 0x300:
+                                print t.move_right, t.move_right, "Application Data from SSL version", t.yellow("v3.0")
+                        except:
+                            pass
+                    elif tls_record_type == 0x16:
+                        for rec in tls_packet.fields['records']:
+                            #print rec['TLS Handshake'].fields['type']
+                            try:
+                                # ClientHello
+                                if rec['TLS Handshake'].fields['type'] == 1:
+                                    #print rec['TLS Client Hello'].fields['version']
+                                    tls_clienthello_version = tls_record['TLS Client Hello'].fields['version']
+                                    if tls_clienthello_version == 0x303:
+                                        print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.2")
+                                    elif tls_clienthello_version == 0x302:
+                                        print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.1")
+                                    elif tls_clienthello_version == 0x301:
+                                        print t.move_right, t.move_right, "Client Hello version", t.yellow("v1.0")
+                                    elif tls_clienthello_version == 0x300:
+                                        print t.move_right, t.move_right, "Client Hello version", t.yellow("v3.0")
+                                # ServerHello
+                                elif rec['TLS Handshake'].fields['type'] == 2:
+                                    #print rec['TLS Server Hello'].fields['version']
+                                    tls_serverhello_version = tls_record['TLS Server Hello'].fields['version']
+                                    if tls_serverhello_version == 0x303:
+                                        print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.2")
+                                    elif tls_serverhello_version == 0x302:
+                                        print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.1")
+                                    elif tls_serverhello_version == 0x301:
+                                        print t.move_right, t.move_right, "Server Hello version", t.yellow("v1.0")
+                                    elif tls_serverhello_version == 0x300:
+                                        print t.move_right, t.move_right, "Server Hello version", t.yellow("v3.0")
+                            except:
+                                pass
             else:
-                pass
+                dbconn.refreshtimestamp(ethsrc)
         except Empty:
             pass
 
